48ed51e9ab9c4e71001135b577ffebe650a13970
tizian-heckmann <tizian.heckmann@tuta.com>
2024-12-10 12:04:10 +0100

    prevent xss by enabling jinja autoescape

--- store/__init__.py
+++ store/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.jinja_env.autoescape = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://dbuser1:7M1BtbB7m1NfBETaBbf85H2H@192.168.2.177:3306/gamestoredb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "super-secret"
